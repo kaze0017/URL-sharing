448d2db64b4adc461cf468b9ef72f983963259ea
Keivan Kazemi <104168492+kaze0017@users.noreply.github.com>
2024-03-20 20:43:11 -0400

    fixes overflows

--- src/app/ui/home/PanelMiddle.tsx
+++ src/app/ui/home/PanelMiddle.tsx
@@ -4,7 +4,7 @@ import PanelMain from "./midPanel/PanelMain";
 
 export default function PanelMiddle() {
   return (
-    <div className="flex flex-col gap-2 w-full h-full">
+    <div className="flex flex-col gap-2 w-full h-full overflow-auto">
       <PanelTop />
       <PanelMain />
     </div>

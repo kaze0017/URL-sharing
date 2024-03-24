dd5c8d3c224a64ef5f3a855a5b340e237d233140
Keivan Kazemi <104168492+kaze0017@users.noreply.github.com>
2024-03-24 13:10:10 -0400

    adds drag and drop

--- src/app/dashboard/membermanagement/page.tsx
+++ src/app/dashboard/membermanagement/page.tsx
@@ -2,32 +2,35 @@
 import React, { useEffect, useState } from "react";
 import { select, hierarchy, tree, linkVertical, drag } from "d3";
 import { getNPeople } from "@/app/lib/actions";
-import { any } from "zod";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { useDrag } from "react-dnd";
+import { useDrop } from "react-dnd";
 
 const people = getNPeople(7);
 
 const orgData = {
-  id: 1,
+  id: 31,
   name: "John Doe",
   photo: people[4].photo,
   children: [
     {
-      id: 2,
+      id: 32,
       name: "Jane Smith",
       photo: people[2].photo,
       children: [
         {
-          id: 3,
+          id: 33,
           name: "John Smith",
           photo: people[3].photo,
           children: [
             {
-              id: 4,
+              id: 34,
               name: "Jane Doe",
               photo: people[4].photo,
             },
             {
-              id: 5,
+              id: 35,
               name: "Jane Doe",
               photo: people[5].photo,
             },
@@ -36,27 +39,27 @@ const orgData = {
       ],
     },
     {
-      id: 6,
+      id: 36,
       name: "Jane Smith",
       photo: people[6].photo,
     },
     {
-      id: 7,
+      id: 37,
       name: "Jane Smith",
       photo: people[3].photo,
       children: [
         {
-          id: 8,
+          id: 38,
           name: "John Smith",
           photo: people[6].photo,
           children: [
             {
-              id: 9,
+              id: 39,
               name: "Jane Doe",
               photo: people[5].photo,
             },
             {
-              id: 10,
+              id: 40,
               name: "Jane Doe",
               photo: people[4].photo,
             },
@@ -76,15 +79,14 @@ interface TreeNode {
 
 interface TreeChartProps {
   data: TreeNode;
-  setData: React.Dispatch<React.SetStateAction<TreeNode>>;
+  draggedData: any;
 }
 
-function TreeChart({ data, setData }: TreeChartProps) {
+function TreeChart({ data, draggedData }: TreeChartProps) {
   const svgRef = React.useRef<SVGSVGElement>(null);
   const [datatoRender, setDatatoRender] = useState<TreeNode>(data);
 
   useEffect(() => {
-    console.log("rendering tree chart");
     const offset = 25;
     const svg = select(svgRef.current);
     svg.selectAll("*").remove();
@@ -168,6 +170,7 @@ function TreeChart({ data, setData }: TreeChartProps) {
       .attr("stroke", "indigo");
 
     // Nodes photo
+
     svg
       .selectAll<SVGImageElement, d3.HierarchyPointNode<any>>("image")
       .data(root.descendants())
@@ -183,6 +186,7 @@ function TreeChart({ data, setData }: TreeChartProps) {
       .attr("data-id", (node) => (node.data as any).id)
       .attr("data-x", (node) => (node as any).x - 25)
       .attr("data-y", (node) => (node as any).y - 25 + offset)
+      .attr("data-type", "node")
       .call(dragHandler as any);
 
     // Add a indigo color trash icon to the bottom right corner of the svg
@@ -198,10 +202,47 @@ function TreeChart({ data, setData }: TreeChartProps) {
       .attr("height", 50);
 
     // Define drag handler function
+    console.log("rendered");
   }, [datatoRender]);
 
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    select("svg").classed("drag-over", true);
+  };
+
+  const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    select("svg").classed("drag-over", false);
+  };
+
+  const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    select("svg").classed("drag-over", false);
+
+    // Retrieve the drag data from the dataTransfer object
+    const dragDataString = e.dataTransfer.getData("application/json");
+    const dragData: DragData = JSON.parse(dragDataString);
+
+    // Access the dragObject from the drag data
+    // console.log("Dropped object name:", dragData.dragObject);
+    // console.log("Dropped on:", e.target);
+    // console.log("Target type:", e.target.getAttribute("data-type"));
+    if (e.target.getAttribute("data-type") === "node") {
+      console.log(dragData.dragObject.id);
+      console.log(e.target.getAttribute("data-id"));
+      const tempData = { ...datatoRender };
+      // Find the node with the id
+      
+    }
+  };
+
   return (
-    <div className="mt-4 p-4">
+    <div
+      className="mt-4 p-4"
+      onDrop={(e) => onDrop(e)}
+      onDragLeave={(e) => onDragLeave(e)}
+      onDragOver={(e) => onDragOver(e)}
+    >
       <svg ref={svgRef} width={900} height={900}>
         {/* Render tree chart here */}
       </svg>
@@ -211,9 +252,106 @@ function TreeChart({ data, setData }: TreeChartProps) {
 
 export default function page() {
   const [data, setData] = useState<TreeNode>(orgData);
+  const [draggedData, setDragData] = useState(null);
+
   return (
     <div>
-      <TreeChart data={data} setData={setData} />
+      <DndProvider backend={HTML5Backend}>
+        <div className="flex">
+          <DraggableBlocks setDragData={(dragData) => setDragData(dragData)} />
+          <TreeChart data={data} draggedData={draggedData} />
+        </div>
+      </DndProvider>
     </div>
   );
 }
+
+// Define interface for block data
+interface Person {
+  id: number;
+  name: string;
+  photo: string;
+}
+
+// Define interface for drag data
+interface DragData {
+  dragObject: Person;
+}
+
+// Define DraggableProps interface
+interface DraggableProps {
+  children: React.ReactNode;
+  dragObject: Person;
+  onDragStart: (dragData: DragData) => void;
+  onDragEnd: () => void;
+}
+
+// Draggable Component
+const Draggable: React.FC<DraggableProps> = ({
+  children,
+  dragObject,
+  onDragStart,
+  onDragEnd,
+}) => {
+  const onDragStarting = (
+    e: React.DragEvent<HTMLDivElement>,
+    dragObject: Person
+  ) => {
+    // Set the data to be transferred
+    e.dataTransfer.setData("application/json", JSON.stringify({ dragObject }));
+
+    // Call the onDragStart handler with the drag data
+    onDragStart({ dragObject });
+  };
+
+  const onDragEnding = (e: React.DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    onDragEnd();
+  };
+
+  return (
+    <div
+      draggable={true}
+      onDragStart={(e) => onDragStarting(e, dragObject)}
+      onDragEnd={onDragEnding}
+    >
+      {children}
+    </div>
+  );
+};
+
+// DraggableBlocks Component
+const DraggableBlocks: React.FC<{
+  setDragData: (dragData: DragData) => void;
+}> = ({ setDragData }) => {
+  const blocks: Person[] = [
+    { id: 21, name: "John Doe", photo: people[1].photo },
+    { id: 22, name: "Jane Smith", photo: people[2].photo },
+    { id: 23, name: "John Smith", photo: people[3].photo },
+    { id: 24, name: "Jane Doe", photo: people[4].photo },
+    { id: 25, name: "Jane Doe", photo: people[5].photo },
+    { id: 26, name: "Jane Doe", photo: people[6].photo },
+  ];
+  const onDragStart = (dragData: DragData) => {
+    setDragData(dragData);
+  };
+
+  const onDragEnd = () => {
+    // Implement any logic needed on drag end
+  };
+
+  return (
+    <div className="dragging-blocks">
+      {blocks.map((b, index) => (
+        <Draggable
+          key={index}
+          dragObject={b}
+          onDragStart={onDragStart}
+          onDragEnd={onDragEnd}
+        >
+          <img src={b.photo} alt={b.name} />
+        </Draggable>
+      ))}
+    </div>
+  );
+};
